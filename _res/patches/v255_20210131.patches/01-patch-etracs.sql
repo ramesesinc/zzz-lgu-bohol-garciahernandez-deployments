8f0d1ad975ddc2b6f058b7cdce8ca7bdc8166870
worgflores <worgflores@gmail.com>
2021-03-24 15:43:13 +0800

    _res: patches: updated

--- _res/patches/v255_20210131.patches/01-patch-etracs.sql
+++ _res/patches/v255_20210131.patches/01-patch-etracs.sql
@@ -95,21 +95,21 @@ update cashreceipt_plugin set `connection` = objid
 
 -- ## 2020-10-27
 
-CREATE TABLE `cashreceipt_rpt_share_forposting` (
-  `objid` varchar(50) NOT NULL,
-  `receiptid` varchar(50) NOT NULL,
-  `rptledgerid` varchar(50) NOT NULL,
-  `txndate` datetime NOT NULL,
-  `error` int(255) NOT NULL,
-  `msg` text,
-  PRIMARY KEY (`objid`),
-  UNIQUE KEY `ux_receiptid_rptledgerid` (`receiptid`,`rptledgerid`),
-  KEY `fk_cashreceipt_rpt_share_forposing_rptledger` (`rptledgerid`),
-  KEY `fk_cashreceipt_rpt_share_forposing_cashreceipt` (`receiptid`),
-  CONSTRAINT `fk_cashreceipt_rpt_share_forposing_cashreceipt` FOREIGN KEY (`receiptid`) REFERENCES `cashreceipt` (`objid`),
-  CONSTRAINT `fk_cashreceipt_rpt_share_forposing_rptledger` FOREIGN KEY (`rptledgerid`) REFERENCES `rptledger` (`objid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-;
+-- CREATE TABLE `cashreceipt_rpt_share_forposting` (
+--   `objid` varchar(50) NOT NULL,
+--   `receiptid` varchar(50) NOT NULL,
+--   `rptledgerid` varchar(50) NOT NULL,
+--   `txndate` datetime NOT NULL,
+--   `error` int(255) NOT NULL,
+--   `msg` text,
+--   PRIMARY KEY (`objid`),
+--   UNIQUE KEY `ux_receiptid_rptledgerid` (`receiptid`,`rptledgerid`),
+--   KEY `fk_cashreceipt_rpt_share_forposing_rptledger` (`rptledgerid`),
+--   KEY `fk_cashreceipt_rpt_share_forposing_cashreceipt` (`receiptid`),
+--   CONSTRAINT `fk_cashreceipt_rpt_share_forposing_cashreceipt` FOREIGN KEY (`receiptid`) REFERENCES `cashreceipt` (`objid`),
+--   CONSTRAINT `fk_cashreceipt_rpt_share_forposing_rptledger` FOREIGN KEY (`rptledgerid`) REFERENCES `rptledger` (`objid`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+-- ;
 
 
 -- ## 2020-11-06
